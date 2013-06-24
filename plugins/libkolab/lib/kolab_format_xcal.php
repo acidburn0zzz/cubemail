f035de27021e0ae81c21e5311c67a681d6b80db1
Aleksander Machniak <machniak@kolabsys.com>
2013-06-24 12:49:33 +0200

    Revert "Fix usage of KolabEvent url setter/getter"
    This reverts commit ba7303572f1d5553900ee5f6789662fbd05f7ffc.

--- plugins/libkolab/lib/kolab_format_xcal.php
+++ plugins/libkolab/lib/kolab_format_xcal.php
@@ -111,7 +111,6 @@ abstract class kolab_format_xcal extends kolab_format
             'title'       => $this->obj->summary(),
             'location'    => $this->obj->location(),
             'description' => $this->obj->description(),
-            'url'         => $this->obj->url(),
             'status'      => $status_map[$this->obj->status()],
             'sensitivity' => $sensitivity_map[$this->obj->classification()],
             'priority'    => $this->obj->priority(),
@@ -244,7 +243,12 @@ abstract class kolab_format_xcal extends kolab_format
         $this->obj->setPriority($object['priority']);
         $this->obj->setClassification($this->sensitivity_map[$object['sensitivity']]);
         $this->obj->setCategories(self::array2vector($object['categories']));
-        $this->obj->setUrl(strval($object['url']));
+
+        $vurls = new vectorurl;
+        foreach ((array)$object['url'] as $url) {
+            $vurls->push(new Url(strval($url)));
+        }
+        $this->obj->setUrls($vurls);
 
         // process event attendees
         $attendees = new vectorattendee;
