c5a2eba629202bc6fb42db770ac828634d110101
Thomas Bruederli <bruederli@kolabsys.com>
2013-07-10 10:09:34 +0200

    Allow to invite/edit attendees to other user's shared events when either a) in a globally shared (anonymous) calendar or b) when enabled by config using the new option 'calendar_allow_invite_shared'
    Fixes bug #1758

--- plugins/calendar/calendar_ui.js
+++ plugins/calendar/calendar_ui.js
@@ -531,17 +531,18 @@ function rcube_calendar_ui(settings)
         $('#edit-recurring-warning').hide();
 
       // init attendees tab
-      var organizer = !event.attendees || is_organizer(event);
+      var organizer = !event.attendees || is_organizer(event),
+        allow_invitations = organizer || (calendar.owner && calendar.owner == 'anonymous') || settings.invite_shared;
       event_attendees = [];
       attendees_list = $('#edit-attendees-table > tbody').html('');
-      $('#edit-attendees-notify')[(notify.checked && organizer ? 'show' : 'hide')]();
-      $('#edit-localchanges-warning')[(has_attendees(event) && !(organizer || (calendar.owner && is_organizer(event, calendar.owner))) ? 'show' : 'hide')]();
+      $('#edit-attendees-notify')[(notify.checked && allow_invitations ? 'show' : 'hide')]();
+      $('#edit-localchanges-warning')[(has_attendees(event) && !(allow_invitations || (calendar.owner && is_organizer(event, calendar.owner))) ? 'show' : 'hide')]();
 
       var load_attendees_tab = function()
       {
         if (event.attendees) {
           for (var j=0; j < event.attendees.length; j++)
-            add_attendee(event.attendees[j], !organizer);
+            add_attendee(event.attendees[j], !allow_invitations);
         }
 
         // select the correct organizer identity
@@ -553,7 +554,7 @@ function rcube_calendar_ui(settings)
           }
         });
         $('#edit-identities-list').val(identity_id);
-        $('#edit-attendees-form')[(organizer?'show':'hide')]();
+        $('#edit-attendees-form')[(allow_invitations?'show':'hide')]();
         $('#edit-attendee-schedule')[(calendar.freebusy?'show':'hide')]();
       };
 
@@ -638,7 +639,7 @@ function rcube_calendar_ui(settings)
           data.attendees = [];
         
         // tell server to send notifications
-        if ((data.attendees.length || (event.id && event.attendees.length)) && organizer && (notify.checked || invite.checked)) {
+        if ((data.attendees.length || (event.id && event.attendees.length)) && allow_invitations && (notify.checked || invite.checked)) {
           data._notify = 1;
         }
 
