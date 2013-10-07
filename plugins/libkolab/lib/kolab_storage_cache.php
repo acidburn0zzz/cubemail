acidburn0zzz/cubemail

commit 8680256c5455ca909a8a2893675699e77ba6cae8

Fix enabled imap threading mode when synchronizing kolab folder (Bug #2323)

plugins/libkolab/lib/kolab_storage_cache.php
@@ -1,4 +1,4 @@
-<?php
+<?php>
 
 /**
  * Kolab storage cache class providing a local caching layer for Kolab groupware objects.
@@ -103,7 +103,7 @@ class kolab_storage_cache
         $this->imap->folder_sync($this->folder->name);
 
         // compare IMAP index with object cache index
-        $imap_index = $this->imap->index($this->folder->name);
+        $imap_index = $this->imap->index($this->folder->name, null, null, true, true);
         $this->index = $imap_index->get();
 
         // determine objects to fetch or to invalidate

commit cc21e9161c6c3de5d5f388b72b563e200216d7f0

Fix typo introduced in commit 8680256c5455ca9

plugins/libkolab/lib/kolab_storage_cache.php
@@ -1,4 +1,4 @@
-<?php>
+<?php
 
 /**
  * Kolab storage cache class providing a local caching layer for Kolab groupware objects.