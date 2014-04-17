e648bee7aac2be5f151618c197757dadf2f05d35
Thomas Bruederli <bruederli@kolabsys.com>
2014-04-17 12:36:09 +0200

    Force cache-rebulding for events and tasks with alarms

--- plugins/libkolab/SQL/mysql/2014040900.sql
+++ plugins/libkolab/SQL/mysql/2014040900.sql
@@ -6,3 +6,11 @@ ALTER TABLE `kolab_cache_note` CHANGE `data` `data` LONGTEXT NOT NULL;
 ALTER TABLE `kolab_cache_file` CHANGE `data` `data` LONGTEXT NOT NULL;
 ALTER TABLE `kolab_cache_configuration` CHANGE `data` `data` LONGTEXT NOT NULL;
 ALTER TABLE `kolab_cache_freebusy` CHANGE `data` `data` LONGTEXT NOT NULL;
+
+-- rebuild cache entries for xcal objects with alarms
+DELETE FROM `kolab_cache_event` WHERE tags LIKE '% x-has-alarms %';
+DELETE FROM `kolab_cache_task` WHERE tags LIKE '% x-has-alarms %';
+
+-- force cache synchronization
+UPDATE `kolab_folders` SET ctag='' WHERE `type` IN ('event','task');
+
