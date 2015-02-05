cea470c81c63df38c732ae55ee90eb3047afce4c
Thomas Bruederli <bruederli@kolabsys.com>
2015-02-05 17:03:11 +0100

    Disable foreign-key checks when dropping tables

--- plugins/libkolab/SQL/mysql.initial.sql
+++ plugins/libkolab/SQL/mysql.initial.sql
@@ -6,6 +6,7 @@
  * @licence GNU AGPL
  **/
 
+/*!40014  SET FOREIGN_KEY_CHECKS=0 */;
 
 DROP TABLE IF EXISTS `kolab_folders`;
 
@@ -185,5 +186,6 @@ CREATE TABLE `kolab_cache_freebusy` (
   INDEX `freebusy_uid2msguid` (`folder_id`,`uid`,`msguid`)
 ) /*!40000 ENGINE=INNODB */ /*!40101 CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
+/*!40014  SET FOREIGN_KEY_CHECKS=1 */;
 
-INSERT INTO `system` (`name`, `value`) VALUES ('libkolab-version', '2015020600');
+REPLACE INTO `system` (`name`, `value`) VALUES ('libkolab-version', '2015020600');
