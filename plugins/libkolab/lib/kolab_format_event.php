6a5a8148348d0eddb452ff33affe4af87e9359cc
Thomas Bruederli <bruederli@kolabsys.com>
2015-02-16 15:59:50 +0100

    Don't remove properties from exceptions which are equal to the master event. KE17 says:
    A recurrence exception SHALL copy ALL properties of the base event, and adjust as required,
    and it SHALL NOT be applied on top of the orginial event properties (The exception replaces
    the complete original event definition for the specific occurrence).

--- plugins/libkolab/lib/kolab_format_event.php
+++ plugins/libkolab/lib/kolab_format_event.php
@@ -241,16 +241,9 @@ class kolab_format_event extends kolab_format_xcal
     private function compact_exception($exception, $master)
     {
         $forbidden = array('recurrence','organizer','_attachments');
-        $whitelist = array('start','end');
 
         foreach ($forbidden as $prop) {
             if (array_key_exists($prop, $exception)) {
-                unset($exception[$prop]);
-            }
-        }
-
-        foreach ($master as $prop => $value) {
-            if (isset($exception[$prop]) && gettype($exception[$prop]) == gettype($value) && $exception[$prop] == $value && !in_array($prop, $whitelist)) {
                 unset($exception[$prop]);
             }
         }
