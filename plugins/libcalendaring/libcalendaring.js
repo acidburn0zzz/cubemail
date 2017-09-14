16a71fccdccdea7df1531603a6ff5ea661fa318f
Aleksander Machniak <machniak@kolabsys.com>
2017-09-14 13:19:32 +0200

    Allow an event updates on iTip request without SEQUENCE bump (Bifrost#T27883)
    We're detecting if something changed in description, title, url,
    location or attendees. These are properties that could be changed
    by the organizer without SEQUENCE bump. If that is detected
    we display "Update in my calendar" button.
    
    TODO: display a diff of changes, so user can take a decission
          if he want's to overwrite his copy of the event.

--- plugins/libcalendaring/libcalendaring.js
+++ plugins/libcalendaring/libcalendaring.js
@@ -1325,6 +1325,8 @@ rcube_libcalendaring.decline_attendee_reply = function(mime_id, task)
  */
 rcube_libcalendaring.fetch_itip_object_status = function(p)
 {
+  p.mbox = rcmail.env.mailbox;
+  p.message_uid = rcmail.env.uid;
   rcmail.http_post(p.task + '/itip-status', { data: p });
 };
 
@@ -1356,7 +1358,7 @@ rcube_libcalendaring.update_itip_object_status = function(p)
   $('#'+p.action+'-'+p.id).show().find('input.button').last().after(p.select);
 
   // highlight date if date change detected
-  if (p.resheduled)
+  if (p.rescheduled)
     $('.calendar-eventdetails td.date').addClass('modified');
 
   // show itip box appendix after replacing the given placeholders
