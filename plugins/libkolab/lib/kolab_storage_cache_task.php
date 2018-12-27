d8180bf560d4317b6e11e2a7dd40512d7e9ee9fe
Aleksander Machniak <machniak@kolabsys.com>
2018-12-27 09:23:29 +0000

    Use cache fast-mode for counting tasks

--- plugins/libkolab/lib/kolab_storage_cache_task.php
+++ plugins/libkolab/lib/kolab_storage_cache_task.php
@@ -24,7 +24,7 @@
 class kolab_storage_cache_task extends kolab_storage_cache
 {
     protected $extra_cols = array('dtstart', 'dtend');
-    protected $data_props = array('categories', 'status'); // start, due
+    protected $data_props = array('categories', 'status', 'complete', 'start', 'due');
 
     /**
      * Helper method to convert the given Kolab object into a dataset to be written to cache
